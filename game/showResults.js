d421497e186c5b8798ae070c101759e09308ea82
StellaGreen <vickysoler5@gmail.com>
2021-04-08 11:13:09 +0200

    add perfectionnism

--- game/showResults.js
+++ game/showResults.js
@@ -3,19 +3,21 @@ const chalk = require('chalk')
 // DEFINED OF LIFE AND POINTS
 let wins = 0;
 let looses = 0;
-exports.showResults = (rest, hidden, wo) => {
+exports.showResults = (rest, hidden, wo,lucky) => {
     if (rest > 0) { // THE PLAYER IS DEAD
       console.log(hidden.join(' '))
       console.log(
         chalk.red.bgBlack(`\n\tHo dude... You are finally a murder\n\tThe word is ${wo}\n`)
       )
       looses++ // DECOMPT OF DEAD
+      lucky--
     } else {
       console.log(hidden.join(' ')); // THE PLAYER AS WIN
       console.log(
         chalk.green.bgBlack(`\nWIN\t\t\tWIN\t\t\tWIN\t\t\tWIN\t\t\tWIN\n\tWIN\t\t\tWIN\t\t\tWIN\t\t\tWIN\t\t\tWIN\n\n\t\t\t\t\tWooow\n\n\t\t\t\tThe answer is ${wo}.`)
       )
       wins++ // DECOMPT OF WIN
+      lucky--
     }
     console.log(chalk.magenta(
       `\n\t\t\t\tGame win ${wins}, ${looses} points loose !\n` // SHOW RESULT OF POINTS
--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-//                         I import the structure of my game 
+const prompt = require('readline-sync')
 const { hangman } = require('./game/gameStructure')
-
+const chalk = require('chalk')
+let chances = 10
 //                            This is the main of my game
-const newGame = () => {
-  hangman()
+
+const resetGame = () => {
+  const chance = chances
+  hangman(chance)
+  chances--
 }
-while (true) {//               We love play in unlimited !
-    newGame();
+if (chances === 10){ // FIRST PLAY
+  console.log(chalk.black.bgGreenBright(`\n\tPress y for play, other letter for stop the game\n`))
+  let retry = prompt.question(chalk.white.bgBlack('\t\tDo you want play a game ?  '))
+  if (retry === "y"){
+    resetGame();
+  }else {
+    process.exit(1)
+  }
+}
+if (chances !== 10){ // AFTER FIRST PLAY
+  console.log(chalk.black.bgGreenBright(`\n\t Press y for play, other letter for stop the game \n`))
+  let retry = prompt.question(chalk.white.bgBlack('\t\t Do you want retry ?  '))
+  if (retry === "y"){
+    resetGame();
+  }else {
+    process.exit(1)
+  }
 }
 //                                     Good Game
